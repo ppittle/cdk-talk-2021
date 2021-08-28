03ad34a03e24ca8bc0a5798eb3d22896a5f37f5b
philip pittle <pittle@amazon.com>
2021-08-28 16:34:54 -0700

    chkpoint 9 - initial domain model and shared infrastructure

---
+++ CloudAutoGroup/Shared/QuoteRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CloudAutoGroup.TVCampaign.Shared
+{
+    public class QuoteRequest
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string CarType { get; set; }
+        public int CreditScoreEstimate { get; set; }
+    }
+}
---
+++ CloudAutoGroup/Shared/QuoteRequestQueueClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CloudAutoGroup.TVCampaign.Shared
+{
+    public interface IQuoteRequestQueueClient
+    {
+        Task Write(QuoteRequest message);
+    }
+
+    public class QuoteRequestQueueClient : IQuoteRequestQueueClient
+    {
+        public Task Write(QuoteRequest message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
