93ec6af75b04441355a5f91abeeb9cde1ab0ce05
philip pittle <pittle@amazon.com>
2021-08-27 17:16:37 -0700

    chkpoint 4 - rename stack

--- CloudAutoGroup/cdk/Program.cs
+++ CloudAutoGroup/cdk/Program.cs
@@ -10,7 +10,7 @@ namespace Cdk
         public static void Main(string[] args)
         {
             var app = new App();
-            new CdkStack(app, "CdkStack", new StackProps
+            new TVCampaignStack(app, "TVCampaignStack", new StackProps
             {
                 // If you don't specify 'env', this stack will be environment-agnostic.
                 // Account/Region-dependent features and context lookups will not work,
