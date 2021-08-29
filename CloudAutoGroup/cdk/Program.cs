25ba37fed5e9b9dfecf6b92090405a2a9116c6df
philip pittle <pittle@amazon.com>
2021-08-28 18:22:49 -0700

    chkpoint 4a - hardcode aws account and region (for now - later will be config driven)

--- CloudAutoGroup/cdk/Program.cs
+++ CloudAutoGroup/cdk/Program.cs
@@ -28,13 +28,12 @@ namespace Cdk
 
                 // Uncomment the next block if you know exactly what Account and Region you
                 // want to deploy the stack to.
-                /*
+                /**/
                 Env = new Amazon.CDK.Environment
                 {
-                    Account = "123456789012",
-                    Region = "us-east-1",
+                    Account = "TODO - Enter your AWS Account Id here",
+                    Region = "eu-west-1",
                 }
-                */
 
                 // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
             });
