8805b273cef32d73aaa06498188dedcf8c3026ea
philip pittle <pittle@amazon.com>
2021-08-28 17:18:57 -0700

    chkpoint 13 - DI Settings into Home Controller

--- CloudAutoGroup/Web/Controllers/HomeController.cs
+++ CloudAutoGroup/Web/Controllers/HomeController.cs
@@ -6,21 +6,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CloudAutoGroup.TVCampaign.Web.Models;
+using Microsoft.Extensions.Options;
 
 namespace CloudAutoGroup.TVCampaign.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IOptions<Settings> _options;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IOptions<Settings> options)
         {
             _logger = logger;
+            _options = options;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_options.Value);
         }
 
         public IActionResult Privacy()
