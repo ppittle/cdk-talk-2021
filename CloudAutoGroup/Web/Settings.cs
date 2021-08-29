cf15754032c8484fc57d6990159ae877d7b2472a
philip pittle <pittle@amazon.com>
2021-08-28 17:17:31 -0700

    chkpoint 12 - Web Settings

---
+++ CloudAutoGroup/Web/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudAutoGroup.TVCampaign.Web
+{
+    public class Settings
+    {
+        public string RequestQuoteApiUrl {get;set;}
+    }
+}
