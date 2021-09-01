86dd279840f6359ae2e1cfceb0614b2f7a373cd5
philip pittle <pittle@amazon.com>
2021-08-31 22:42:00 -0700

    chkpoint 34 - quotes api controller

---
+++ CloudAutoGroup/Web/Controllers/QuotesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CloudAutoGroup.TVCampaign.Shared;
+
+namespace CloudAutoGroup.TVCampaign.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private readonly IFullQuoteRepository _quoteRepository;
+
+        public QuotesController(IFullQuoteRepository quoteRepository)
+        {
+            _quoteRepository = quoteRepository;
+        }
+
+        // GET: api/Quotes/
+        [HttpGet]
+        public async Task<IEnumerable<FullQuote>> Get()
+        {
+            return await _quoteRepository.GetAll();
+        }
+    }
+}
