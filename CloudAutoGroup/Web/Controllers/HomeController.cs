4090a2f124dcfd1aa21e1c7acd4592b0acb8a1a2
philip pittle <pittle@amazon.com>
2021-08-31 22:29:44 -0700

    chkpoint 32 - create empty Dashboard page

--- CloudAutoGroup/Web/Controllers/HomeController.cs
+++ CloudAutoGroup/Web/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace CloudAutoGroup.TVCampaign.Web.Controllers
             return View();
         }
 
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
